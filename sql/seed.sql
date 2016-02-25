21b68ac7f61a196ed463f29f9ac10d97853b09ea
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-02-24 20:56:36 -0700

    values in subjects, parents, and teachers

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,44 +1,94 @@
-CREATE TABLE parents (
-  id serial,
-  name varchar(255)
-);
+INSERT INTO parents VALUES 
+(default, 'Autumn Hirthe'),
+(default, 'Christian Carroll'),
+(default, 'Monty Prohaska'),
+(default, 'Montana Bayer'),
+(default, 'Dawson Tromp'),
+(default, 'Bret Windler'),
+(default, 'Quentin Ledner'),
+(default, 'Abigail Lueilwitz'),
+(default, 'Sage Murazik'),
+(default, 'Madalyn Eichmann'),
+(default, 'Dorthy Schmeler'),
+(default, 'Alex Metz'),
+(default, 'Rachael Heidenreich'),
+(default, 'Hanna Wisoky'),
+(default, 'Samantha Lemke'),
+(default, 'Shawn Welch'),
+(default, 'Haylie Ortiz'),
+(default, 'Alison Flatley'),
+(default, 'Zackary Bergstrom'),
+(default, 'Virginie Hudson'),
+(default, 'Abdullah Schultz'),
+(default, 'Dillon Anderson'),
+(default, 'Bailee Bashirian'),
+(default, 'Cassandra Balistreri'),
+(default, 'Catalina Sanford'),
+(default, 'Maud Konopelski'),
+(default, 'Gina Morissette'),
+(default, 'Johann Sauer'),
+(default, 'Reuben Kuhic'),
+(default, 'Jewel Bergstrom'),
+(default, 'Hosea Metz'),
+(default, 'Alaina Armstrong'),
+(default, 'Lorena Kuphal'),
+(default, 'Ashly Hand'),
+(default, 'Cleta Watsica'),
+(default, 'Nikita Larson'),
+(default, 'Trace Herzog'),
+(default, 'Dolores Labadie'),
+(default, 'Evan Hackett'),
+(default, 'Nathanael Hayes'),
+(default, 'Junius Hirthe'),
+(default, 'Idella Hickle'),
+(default, 'Fausto Swaniawski'),
+(default, 'Patience Hilpert'),
+(default, 'Saige Christiansen'),
+(default, 'Isai Jacobson'),
+(default, 'Garrick Harvey'),
+(default, 'Furman Kertzmann'),
+(default, 'Nola Tillman'),
+(default, 'Darron Cremin'),
+(default, 'Dulce Heathcote')
 
-CREATE TABLE teachers (
-  id serial,
-  name varchar(255)
-);
+INSERT INTO teachers VALUES
+(default, 'Shirley Toy'),
+(default, 'Baron Mohr'),
+(default, 'Georgiana Watsica'),
+(default, 'Ernestine Carroll'),
+(default, 'Mitchell Kunde'),
+(default, 'Haven Ritchie'),
+(default, 'Charlotte Ratke'),
+(default, 'Haylie Kshlerin'),
+(default, 'Ceasar Wolff'),
+(default, 'Davin Hintz'),
+(default, 'Hanna Gleichner'),
+(default, 'Rocky Hartmann'),
+(default, 'Elda Schaefer'),
+(default, 'Tracey Stoltenberg'),
+(default, 'Gerardo Kilback'),
+(default, 'Theron Heathcote'),
+(default, 'Easter Schoen'),
+(default, 'Christa Greenholt'),
+(default, 'Jodie Emmerich'),
+(default, 'Theo Hyatt'),
+(default, 'Cara Dickinson'),
+(default, 'Bernadette Barton'),
+(default, 'Drake Schroeder'),
+(default, 'Preston Kreiger'),
+(default, 'Angelita Price'),
+(default, 'Hugh Kunde'),
+(default, 'Helena Muller')
 
-CREATE TABLE subjects (
-  id serial,
-  name varchar(255)
-);
+INSERT INTO subjects VALUES
+(default, 'Transfiguration'),
+(default, 'Charms'),
+(default, 'Potions'),
+(default, 'History of Magic'),
+(default, 'Defence Against the Dark Arts'),
+(default, 'Astronomy'),
+(default, 'Herbology'),
+(default, 'Flying')
 
-CREATE TABLE classes (
-  id serial primary key,
-  name varchar(70),
-  subject_id integer references subjects(id) on delete cascade,
-  teacher_id integer references teachers(id) on delete cascade
-);
 
-CREATE TABLE houses (
- id serial primary key,
- name varchar(70),
- teacher_id integer references teachers(id) on delete cascade
-);
 
-CREATE TABLE students (
- id serial primary key,
- name varchar(255),
- year integer,
- house_id integer references houses(id) on delete cascade
-);
-
-CREATE TABLE schedules (
-  class_id integer references classes(id) on delete cascade
-  student_id integer references students(id) on delete cascade
-);
-
-CREATE TABLE families (
-  parent_id integer references parents(id) on delete cascade
-  student_id integer references students(id) on delete cascade
-);
