6899933dd6d865a08ae83451408ddbd9e47561b3
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-02-24 20:26:16 -0700

    tables created

---
+++ sql/seed.sql
@@ -0,0 +1,44 @@
+CREATE TABLE parents (
+  id serial,
+  name varchar(255)
+);
+
+CREATE TABLE teachers (
+  id serial,
+  name varchar(255)
+);
+
+CREATE TABLE subjects (
+  id serial,
+  name varchar(255)
+);
+
+CREATE TABLE classes (
+  id serial primary key,
+  name varchar(70),
+  subject_id integer references subjects(id) on delete cascade,
+  teacher_id integer references teachers(id) on delete cascade
+);
+
+CREATE TABLE houses (
+ id serial primary key,
+ name varchar(70),
+ teacher_id integer references teachers(id) on delete cascade
+);
+
+CREATE TABLE students (
+ id serial primary key,
+ name varchar(255),
+ year integer,
+ house_id integer references houses(id) on delete cascade
+);
+
+CREATE TABLE schedules (
+  class_id integer references classes(id) on delete cascade
+  student_id integer references students(id) on delete cascade
+);
+
+CREATE TABLE families (
+  parent_id integer references parents(id) on delete cascade
+  student_id integer references students(id) on delete cascade
+);
