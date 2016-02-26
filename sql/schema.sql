95a847837916fc120f0ac4b89d2dd0ba8c6b6832
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-02-26 15:30:06 -0700

    fixed making tables/database and seeded it with info from seed.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,47 +1,49 @@
-DROP DATABASE IF EXISTS hogwarts_Kyle_Maxwell;
-CREATE DATABASE hogwarts_Kyle_Maxwell;
+DROP DATABASE IF EXISTS hogwarts_kyle_maxwell;
+CREATE DATABASE hogwarts_kyle_maxwell;
+
+\connect hogwarts_kyle_maxwell;
 
 CREATE TABLE parents (
-  id serial,
+  id serial PRIMARY KEY,
   name varchar(255)
 );
 
 CREATE TABLE teachers (
-  id serial,
+  id serial PRIMARY KEY,
   name varchar(255)
 );
 
 CREATE TABLE subjects (
-  id serial,
+  id serial PRIMARY KEY,
   name varchar(255)
 );
 
 CREATE TABLE classes (
-  id serial primary key,
+  id serial PRIMARY KEY,
   name varchar(70),
   subject_id integer references subjects(id) on delete cascade,
   teacher_id integer references teachers(id) on delete cascade
 );
 
 CREATE TABLE houses (
- id serial primary key,
+ id serial PRIMARY KEY,
  name varchar(70),
  teacher_id integer references teachers(id) on delete cascade
 );
 
 CREATE TABLE students (
- id serial primary key,
+ id serial PRIMARY KEY,
  name varchar(255),
  year integer,
  house_id integer references houses(id) on delete cascade
 );
 
 CREATE TABLE schedules (
-  class_id integer references classes(id) on delete cascade
+  class_id integer references classes(id) on delete cascade,
   student_id integer references students(id) on delete cascade
 );
 
 CREATE TABLE families (
-  parent_id integer references parents(id) on delete cascade
+  parent_id integer references parents(id) on delete cascade,
   student_id integer references students(id) on delete cascade
 );
