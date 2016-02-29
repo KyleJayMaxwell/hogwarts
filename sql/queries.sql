1b71fa1528f773897e8dcfe4e98ef53a46e3ab6b
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-02-28 18:42:45 -0700

    students taking classes by a given teacher, and given year

---
+++ sql/queries.sql
@@ -0,0 +1,27 @@
+-- How many students go to hogwarts?
+SELECT id FROM students;
+
+-- How many students are in a specific house?
+SELECT * FROM students WHERE house_id = 1;
+
+-- How many students are taking a class by a given teacher?
+SELECT * FROM schedules
+JOIN students
+ON schedules.student_id = students.id
+JOIN classes
+ON classes.id = schedules.class_id
+JOIN teachers
+ON teachers.id = classes.teacher_id
+WHERE teachers.name LIKE '%Georgiana Watsica%';
+-- OR
+SELECT * FROM schedules
+JOIN students
+ON schedules.student_id = students.id
+JOIN classes
+ON classes.id = schedules.class_id
+WHERE teacher_id = 3;
+
+-- How many students are of a given year?
+SELECT * FROM students WHERE year = 2;
+
+
